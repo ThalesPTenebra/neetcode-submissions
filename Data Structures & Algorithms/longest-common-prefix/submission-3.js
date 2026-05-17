6c3c3b70759572126a450f80c8e16fb8247bcdce
ThalesPTenebra <71927645+ThalesPTenebra@users.noreply.github.com>
2026-05-17 14:18:45 -0300

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+        // Pointer in position i and check if every word in strs has equal strs[i]
+        let longestPrefix = ""
+        let i = 0;
+        while(i < strs[0].length){
+            let currentChar = strs[0][i]
+            for(const word of strs){
+                if(word[i] !== currentChar) return longestPrefix
+            }
+
+            longestPrefix += strs[0][i]
+            i++;
+        }
+        
+
+        return longestPrefix
+    }
+}
